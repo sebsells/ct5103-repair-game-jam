4348ab76eb129590c3cfc8552a4ea6d4ce25a3c5
sebsells <65076232+sebsells@users.noreply.github.com>
2022-05-07 22:07:07 +0100

    Player can now aim and shoot

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField] float speed = 40.0f; // Speed of the bullet
+    [SerializeField] float maxTime = 2.0f; // Maximum time the bullet will exist for
+    float currentTime = 0.0f; // Current time bullet has existed for
+
+    void Update()
+    {
+        // Check if bullet has existed for too long
+        currentTime += Time.deltaTime;
+        if (currentTime >= maxTime)
+        {
+            Destroy(gameObject);
+        }
+
+        // Move bullet
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,23 +4,58 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    float speedBase = 10;     // Default speed when moving
+    [SerializeField] GameObject bullet; // Bullet prefab that will be fired
+    float bulletCooldown = 0.3f; // Time between each bullet fire
+    float bulletLastFired = 0.0f; // Time when bullet was last fired
+
+    bool isAiming = false; // True if the player is aiming with arrow keys or right joystick
+
+    float speed = 0.0f;       // Calculated speed of the player
+    float speedBase = 10.0f;  // Default speed when moving
     float speedSprint = 1.5f; // Multiplier for speed when sprinting
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0.0f || Input.GetAxisRaw("Vertical") != 0.0f)
+        // AIMING
+        if (Input.GetAxisRaw("AimH") != 0.0f || Input.GetAxisRaw("AimV") != 0.0f)
+        {
+            isAiming = true;
+
+            float angle = Mathf.Atan2(Input.GetAxisRaw("AimH"), Input.GetAxisRaw("AimV")) * Mathf.Rad2Deg;
+            transform.Rotate(new Vector3(0.0f, angle, 0.0f) - transform.rotation.eulerAngles);
+        }
+
+        // FIRING
+        if (Input.GetAxisRaw("Fire") != 0.0f)
+        {
+            if (bulletLastFired + bulletCooldown <= Time.time)
+            {
+                Vector3 bulletStartPos = new Vector3(Input.GetAxisRaw("AimH"), 0.0f, Input.GetAxisRaw("AimV")) * 0.5f; // Getting the position of the players "gun"
+                Instantiate(bullet, transform.position + bulletStartPos, transform.rotation); // Instantiating bullet
+                bulletLastFired = Time.time; // Resetting bullet timer
+            }
+        }
+
+        // MOVEMENT
+        if (Input.GetAxisRaw("MoveH") != 0.0f || Input.GetAxisRaw("MoveV") != 0.0f)
         {
             // Getting the direction that the player is moving as a normalised vector (only really matters if they are using a controller)
-            Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
-            direction.Normalize();
+            Vector3 directionV3 = new Vector3(Input.GetAxisRaw("MoveH"), 0.0f, Input.GetAxisRaw("MoveV"));
+            directionV3.Normalize();
 
-            // Turning the player
+            // Turning the player to the direction they are moving (if not aiming)
+            if (!isAiming)
+            {
+                float angle = Mathf.Atan2(Input.GetAxisRaw("MoveH"), Input.GetAxisRaw("MoveV")) * Mathf.Rad2Deg;
+                transform.Rotate(new Vector3(0.0f, angle, 0.0f) - transform.rotation.eulerAngles);
+            }
 
 
             // Moving the player
-            transform.position += (direction * speedBase * Time.deltaTime);
+            transform.Translate(directionV3 * speedBase * Time.deltaTime, Space.World);
         }
+
+        isAiming = false;
     }
 }
